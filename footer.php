143cb00f8a8bd1259866ae5c618b859b5f1be3b7
mzylka <m.zylka1311@gmail.com>
2020-02-04 19:56:55 +0100

    Remove dot in register_style and change footer content

--- footer.php
+++ footer.php
@@ -8,9 +8,9 @@
                 <h3>Hurtownia Antyków</h3>
                 <ul>
                     <li>Kołobrzeg 78-100, ul. Wylotowa 87</li>
-                    <li>(+48)606 637 713</li>
+                    <li>tel: (+48)606 637 713</li>
                     <li>Godziny otwarcia: 10.00-18.00</li>
-                    <li>fanta001@interia.pl</li>
+                    <li>e-mail: fanta001@interia.pl</li>
                 </ul>
                 <div class="footer__map">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1167.584606190323!2d15.55044948368015!3d54.177078997520205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470015a728f84cb3%3A0xb7677d630c8650fd!2santyki%20wylotowa!5e0!3m2!1spl!2spl!4v1579191151493!5m2!1spl!2spl" width="auto" height="250px" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function load_stylesheets(){
-    wp_register_style('stylesheet', get_template_directory_uri() . './style.css', '', 1, 'all');
+    wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', '', 1, 'all');
     wp_enqueue_style('stylesheet');
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
